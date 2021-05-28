b1c1a463bd8f59572d89a57135f8d83009a84bfb
Muhammadqodir <m.qodir777@gmail.com>
2021-05-29 00:46:53 +0300

    Created main menu

--- BrailleRecognition-Android/app/src/main/java/uz/mq/braillerecognition/MainActivity.java
+++ BrailleRecognition-Android/app/src/main/java/uz/mq/braillerecognition/MainActivity.java
@@ -317,7 +317,44 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        //switch case
+
+        switch (menuItem.getItemId()){
+            case R.id.nav_favorite:
+//                startActivity(new Intent(MainActivity.this, FavoritesActivity.class));
+                break;
+            case R.id.nav_info:
+                showInfoDialog();
+                break;
+            case R.id.nav_share:
+                try {
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Braille Recognition");
+                    String shareMessage= "\nBraille Recognition\n"+getResources().getString(R.string.app_share);
+                    shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+                    startActivity(Intent.createChooser(shareIntent, "Select"));
+                } catch(Exception e) {
+                    //e.toString();
+                }
+                break;
+            case R.id.nav_rate:
+                Uri uri = Uri.parse("market://details?id=" + getPackageName());
+
+                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                // To count with Play market backstack, After pressing back button,
+                // to taken back to our application, we need to add following flags to intent.
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                try {
+                    startActivity(goToMarket);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                }
+                break;
+        }
         drawer.close();
         return false;
     }
